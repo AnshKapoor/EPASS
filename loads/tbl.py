f8f2a155c1c7f7f1cc99651e9497c80d738fab47
Christopher Blech <blech@ina.ing.tu-bs.de>
2019-11-04 12:47:57 +0100

    vtk actors updated for tbl load

--- loads/tbl.py
+++ loads/tbl.py
@@ -1,11 +1,12 @@
 #
+import json
 from PyQt5.QtWidgets import QApplication, QLabel, QWidgetItem, QCheckBox, QLineEdit
 import vtk
 import numpy as np
 import math
 import os
 from lxml import etree
-from standardWidgets import removeButton, editButton, setupWindow, messageboxOK, progressWindow
+from standardWidgets import ak3LoadButton, removeButton, editButton, setupWindow, messageboxOK, progressWindow
 from loads import load
 
 # tbl load
@@ -18,20 +19,22 @@ class tbl(load):
         self.editButton = editButton()
         self.type = 'Turbulent Boundary Layer'
         #
-        self.amp = QLineEdit('1.')
-        self.dirX = QLineEdit('1.')
-        self.dirY = QLineEdit('1.')
+        self.dirX = QLineEdit('0.')
+        self.dirY = QLineEdit('0.')
         self.dirZ = QLineEdit('1.')
-        self.c = QLineEdit('340.')
+        self.flowDirX = QLineEdit('1.')
+        self.flowDirY = QLineEdit('0.')
+        self.flowDirZ = QLineEdit('0.')
+        self.loadButton = ak3LoadButton(self.ak3path)
+        self.loadButton.clicked.connect(self.getFilename)
         #
         self.label = QLabel('Turbulent Boundary Layer')
-        self.ampLabel = QLabel(self.amp.text() + ' Pa')
-        self.dirLabel = QLabel('x ' + self.dirX.text() + ' y ' + self.dirY.text() + ' z ' + self.dirZ.text())
+        self.dirLabel = QLabel('x ' + self.flowDirX.text() + ' y ' + self.flowDirY.text() + ' z ' + self.flowDirZ.text())
         self.drawCheck = QCheckBox('Draw')
         self.drawCheck.setStatusTip('Show load in 2D Graph and 3D Window')
         self.drawCheck.clicked.connect(self.switch)
         #
-        [self.addWidget(wid) for wid in [self.removeButton, self.label, self.ampLabel, self.dirLabel, self.drawCheck, self.editButton]]
+        [self.addWidget(wid) for wid in [self.removeButton, self.label, self.dirLabel, self.drawCheck, self.editButton]]
         #
         self.initSetupWindow()
         self.init3DActor(vtkWindow)
@@ -44,7 +47,6 @@ class tbl(load):
         if var == 0: # is the case if the initial setup window is canceled by the user
             self.generatePressure()
             self.update3DActor()
-
     #
     def clearLayout(self):
         """
@@ -55,45 +57,51 @@ class tbl(load):
                 self.takeAt(i).widget().setParent(None)
             else:
                 self.removeItem(self.contLayout.takeAt(i))
-
     #
     def generatePressure(self):
         """
-        Calculates pressure excitation on the selected blocks due to the created plane wave
+        Calculates pressure excitation on the selected blocks according to Klabes 2017 (autospectrum) and Efimtsov (Wavenumber-spectrum) due to a turbulent boundary layer
         """
-        c = float(self.c.text())
         frequencies = self.myModel.calculationObjects[0].frequencies
         self.findRelevantPoints()
-        #print(self.surfacePoints)
         if self.surfacePoints is not []:
             self.surfacePhases = np.zeros((len(frequencies),len(self.surfacePoints)))
             r_vector = []
-             # Get model infos
+            # Get model infos
             nodes = self.myModel.calculationObjects[0].nodes
             center = [0.5*(max(nodes[:,1]) + min(nodes[:,1])), 0.5*(max(nodes[:,2]) + min(nodes[:,2])), 0.5*(max(nodes[:,3]) + min(nodes[:,3]))]
-            loadNormal = [float(self.dirX.text()), float(self.dirY.text()), float(self.dirZ.text())]
-            loadNormal = loadNormal/np.linalg.norm(loadNormal)
-            # calculate distances in direction of the plane wave
+            flowDir = [float(self.flowDirX.text()), float(self.flowDirY.text()), float(self.flowDirZ.text())]
+            flowDir = flowDir/np.linalg.norm(flowDir)
+            # Calculate distances in direction of the flow
             progWin = progressWindow(len(self.surfacePoints)-1, "Calculating distances")
             for nsp, surfacePoint in enumerate(self.surfacePoints):
-                r_vector.append(abs(loadNormal[0]*(surfacePoint[0] - center[0]) + loadNormal[1]*(surfacePoint[1] - center[1]) + loadNormal[2]*(surfacePoint[2] - center[2])))
+                r_vector.append(abs(flowDir[0]*(surfacePoint[0] - center[0]) + flowDir[1]*(surfacePoint[1] - center[1]) + flowDir[2]*(surfacePoint[2] - center[2])))
                 progWin.setValue(nsp)
                 QApplication.processEvents()
-            # calculate the correct phases for the distances and apply the amp requested by the user in loadNormal direction to each element
+            # Calculate phases for the distances using Efimtsov model 
             progWin = progressWindow(len(frequencies)-1, "Calculating phases")
             for nf, f in enumerate(frequencies):
-                lam = c/f # wave length
+                lam = 1./f # wave length
                 self.surfacePhases[nf,:] = [2*math.pi*(r % lam)/lam for r in r_vector]
                 progWin.setValue(nf)
                 QApplication.processEvents()
-
+    #
+    def getFilename(self):
+        """
+        Open menu to choose file which contains point data for tbl parameters
+        """
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(None,"QFileDialog.getOpenFileName()", "","All Files (*);;json Files (*.json)", options=options)
+        if fileName:
+            self.filename = fileName
+        datanew = self.loadData(fileName) #calls loadData function to read the file
     #
     def getXYdata(self):
         """
         Return x, y data for plotting; for plane wave: constant amplitude
         """
-        return self.myModel.calculationObjects[0].frequencies, len(self.myModel.calculationObjects[0].frequencies)*[float(self.amp.text())]
-
+        return self.myModel.calculationObjects[0].frequencies, len(self.myModel.calculationObjects[0].frequencies)*[1.]
     #
     def init3DActor(self, vtkWindow):
         """
@@ -104,8 +112,10 @@ class tbl(load):
         center = [0.5*(max(nodes[:,1]) + min(nodes[:,1])), 0.5*(max(nodes[:,2]) + min(nodes[:,2])), 0.5*(max(nodes[:,3]) + min(nodes[:,3]))]
         loadNormal = [float(self.dirX.text()), float(self.dirY.text()), float(self.dirZ.text())]
         loadNormal = loadNormal/np.linalg.norm(loadNormal)
+        flowDir = [float(self.flowDirX.text()), float(self.flowDirY.text()), float(self.flowDirZ.text())]
+        flowDir = flowDir/np.linalg.norm(flowDir)
         scaleFactor = max( [abs(max(nodes[:,1])-min(nodes[:,1])), abs(max(nodes[:,2])-min(nodes[:,2])), abs(max(nodes[:,3])-min(nodes[:,3]))] )
-        # Symbol for plane wave
+        # Symbol for normal direction of load
         # Calculate random directions in the plane
         direction1 = np.cross(loadNormal, [loadNormal[0]+0.11*loadNormal[1], loadNormal[1]+0.22*loadNormal[2], loadNormal[2]+0.33*loadNormal[0]])
         direction1 = direction1/np.linalg.norm(direction1)
@@ -128,14 +138,16 @@ class tbl(load):
         self.loadActorSymbol.GetProperty().SetAmbient(0.9);
         self.loadActorSymbol.GetProperty().SetDiffuse(0.1);
         self.loadActorSymbol.GetProperty().SetSpecular(0.);
-        # Arrow for direction of load
+        # Arrow for normal direction of load and flow direction (3 arrows)
         self.arrowDataSymbol = vtk.vtkPolyData()
         arrowPointsSymbol = vtk.vtkPoints()
         arrowPointsSymbol.InsertNextPoint(center[0] + scaleFactor*loadNormal[0], center[1] + scaleFactor*loadNormal[1], center[2] + scaleFactor*loadNormal[2])
+        arrowPointsSymbol.InsertNextPoint(center[0] + scaleFactor*flowDir[0],    center[1] + scaleFactor*flowDir[1],    center[2] + scaleFactor*flowDir[2])
         self.arrowDataSymbol.SetPoints(arrowPointsSymbol)
         self.arrowVectorsSymbol = vtk.vtkDoubleArray()
         self.arrowVectorsSymbol.SetNumberOfComponents(3)
         self.arrowVectorsSymbol.InsertNextTuple([scaleFactor*-0.2*loadNormal[0], scaleFactor*-0.2*loadNormal[1], scaleFactor*-0.2*loadNormal[2]])
+        self.arrowVectorsSymbol.InsertNextTuple([scaleFactor*-0.2*flowDir[0],    scaleFactor*-0.2*flowDir[1],    scaleFactor*-0.2*flowDir[2]])
         self.arrowDataSymbol.GetPointData().SetVectors(self.arrowVectorsSymbol)
         arrowSource = vtk.vtkArrowSource()
         # Glyph for load symbol
@@ -174,22 +186,20 @@ class tbl(load):
 
         #List of Actors for iteration in vtkWindow
         self.actorsList = [self.arrowActorLoad, self.arrowActorSymbol, self.loadActorSymbol]
-
-
-
-
-
+    #
     def initSetupWindow(self):
         """
         basic objects for the individual setup window
         """
         self.setupWindow = setupWindow(self.label.text())
         # ADD TO LAYOUT
-        self.setupWindow.layout.addRow(QLabel('Amplitude'), self.amp)
-        self.setupWindow.layout.addRow(QLabel('Direction X'), self.dirX)
-        self.setupWindow.layout.addRow(QLabel('Direction Y'), self.dirY)
-        self.setupWindow.layout.addRow(QLabel('Direction Z'), self.dirZ)
-        self.setupWindow.layout.addRow(QLabel('Speed of Sound'), self.c)
+        self.setupWindow.layout.addRow(QLabel('Norm-Direction X'), self.dirX)
+        self.setupWindow.layout.addRow(QLabel('Norm-Direction Y'), self.dirY)
+        self.setupWindow.layout.addRow(QLabel('Norm-Direction Z'), self.dirZ)
+        self.setupWindow.layout.addRow(QLabel('Flow-direction X'), self.flowDirX)
+        self.setupWindow.layout.addRow(QLabel('Flow-direction Y'), self.flowDirY)
+        self.setupWindow.layout.addRow(QLabel('Flow-direction Z'), self.flowDirZ)
+        self.setupWindow.layout.addRow(QLabel('Load xyz input file'), self.loadButton)
         #
         self.blockChecker = []
         for block in self.myModel.calculationObjects[0].elems:
@@ -197,13 +207,13 @@ class tbl(load):
             self.setupWindow.blockLayout.addRow(self.blockChecker[-1], QLabel('Block ' + str(block[1]) + ' (' + str(block[0]) + ')'))
         #
         self.setupWindow.setFixedSize(self.setupWindow.mainLayout.sizeHint())
-
+    #
     def resetValues(self):
-        for n, item in enumerate([self.amp, self.dirX, self.dirY, self.dirZ, self.c]):
+        for n, item in enumerate([self.dirX, self.dirY, self.dirZ, self.flowDirX, self.flowDirY, self.flowDirZ]):
             item.setText(self.varSave[n])
-
+    #
     def showEdit(self):
-        self.varSave = [self.amp.text(), self.dirX.text(), self.dirY.text(), self.dirZ.text(), self.c.text()]
+        self.varSave = [self.dirX.text(), self.dirY.text(), self.dirZ.text(), self.flowDirX.text(), self.flowDirY.text(), self.flowDirZ.text()]
         var = self.setupWindow.exec_()
         if var == 0: # reset values
             self.resetValues()
@@ -211,9 +221,9 @@ class tbl(load):
             try:
                 if float(self.dirX.text()) == 0. and float(self.dirY.text()) == 0. and float(self.dirZ.text()) == 0.:
                     raise Exception
-                self.ampLabel.setText(str(float(self.amp.text())) + ' Pa')
-                self.dirLabel.setText('x ' + str(float(self.dirX.text())) + ' y ' + str(float(self.dirY.text())) + ' z ' + str(float(self.dirZ.text())))
-                c = float(self.c.text()) # It's just a check, variable is not used here
+                if float(self.flowDirX.text()) == 0. and float(self.flowDirY.text()) == 0. and float(self.flowDirZ.text()) == 0.:
+                    raise Exception
+                self.dirLabel.setText('x ' + self.flowDirX.text() + ' y ' + self.flowDirY.text() + ' z ' + self.flowDirZ.text())
                 self.generatePressure()
                 self.update3DActor()
                 self.switch()
@@ -230,13 +240,15 @@ class tbl(load):
             self.changeSwitch.setChecked(0)
         else:
             self.changeSwitch.setChecked(1)
-
+    #
     def update3DActor(self):
         # Get model infos
         nodes = self.myModel.calculationObjects[0].nodes
         center = [0.5*(max(nodes[:,1]) + min(nodes[:,1])), 0.5*(max(nodes[:,2]) + min(nodes[:,2])), 0.5*(max(nodes[:,3]) + min(nodes[:,3]))]
         loadNormal = [float(self.dirX.text()), float(self.dirY.text()), float(self.dirZ.text())]
         loadNormal = loadNormal/np.linalg.norm(loadNormal)
+        flowDir = [float(self.flowDirX.text()), float(self.flowDirY.text()), float(self.flowDirZ.text())]
+        flowDir = flowDir/np.linalg.norm(flowDir)
         scaleFactor = max( [abs(max(nodes[:,1])-min(nodes[:,1])), abs(max(nodes[:,2])-min(nodes[:,2])), abs(max(nodes[:,3])-min(nodes[:,3]))] )
         # Symbol for plane wave
         # Calculate random directions in the plane
@@ -249,8 +261,10 @@ class tbl(load):
         self.loadGridSymbol.GetPoints().Modified()
         # Update load symbol
         self.arrowDataSymbol.GetPoints().SetPoint(0, center[0] + scaleFactor*loadNormal[0], center[1] + scaleFactor*loadNormal[1], center[2] + scaleFactor*loadNormal[2])
+        self.arrowDataSymbol.GetPoints().SetPoint(1, center[0] + scaleFactor*flowDir[0],    center[1] + scaleFactor*flowDir[1],    center[2] + scaleFactor*flowDir[2])
         self.arrowDataSymbol.GetPoints().Modified()
         self.arrowVectorsSymbol.SetTuple(0, [scaleFactor*-0.2*loadNormal[0], scaleFactor*-0.2*loadNormal[1], scaleFactor*-0.2*loadNormal[2]])
+        self.arrowVectorsSymbol.SetTuple(1, [scaleFactor*-0.2*flowDir[0],    scaleFactor*-0.2*flowDir[1],    scaleFactor*-0.2*flowDir[2]])
         self.arrowVectorsSymbol.Modified()
         # Update load
         arrowPointLoad = vtk.vtkPoints()
@@ -261,8 +275,7 @@ class tbl(load):
         [arrowVectorsLoad.InsertNextTuple([-0.1*scaleFactor*vec[0], -0.1*scaleFactor*vec[1], -0.1*scaleFactor*vec[2]]) for vec in self.surfaceElementNormals]
         self.arrowDataLoad.GetPointData().SetVectors(arrowVectorsLoad)
         self.arrowDataLoad.Modified()
-        #
-
+    #
     def writeXML(self, exportAK3, name):
         elemLoads = exportAK3.find('ElemLoads')
         oldNoOfLoads = elemLoads.get('N')
