a15c2417fa47c17c5d4bae4ce2025c2c01a6a8e7
Harikrishnan Sreekumar <hk.sreekumar@tu-braunschweig.de>
2022-09-16 16:51:42 +0200

    Added example script

---
+++ examples/scripts/modelscript_basic.py
@@ -0,0 +1,9 @@
+## usage: python3 .\main.py --cmd --script .\examples\scripts\modelscript_basic.py
+
+tool.loadInput('./Plate_Shell4_Free_on_all_sides.cub5')
+tool.setFrequency(9, 999, 99)
+#tool.addLoad('Point force', '0.,0.,0.,0.,1.')
+#tool.addMaterial()
+#tool.assignMaterialToBlock()
+#tool.assignElementToBlock()
+#tool.addConstraint()
